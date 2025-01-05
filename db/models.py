8f8cab2d987fe85b101372db1ac7a6b5df1eba86
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-01-05 07:08:44 +0300

    Add model Document of data base

---
+++ db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Boolean
+from .db_core import Base
+
+
+class Documents(Base):
+
+    __tablename__ = 'documents'
+    name = Column(String, index=True, primary_key=True)
+    is_income = Column(Boolean)
+    path = Column(String, unique=True)
+
+    def __str__(self):
+        return f'{self.name}; type={'income' if self.is_income else 'outcome'}'
