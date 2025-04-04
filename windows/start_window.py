04b3e22f3a41b4384c8abef260d345534866cb12
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-04-05 02:19:10 +0300

    Set background as customer image

--- windows/start_window.py
+++ windows/start_window.py
@@ -7,14 +7,14 @@ class StartWindow(tk.Tk):
 
     def __init__(
             self,
-            width: int = 900,
+            width: int = 750,
             height: int = 600,
             program_name: str = 'Программа для учета документов'
     ):
         super().__init__()
         self.geometry(f'{width}x{height}')
         self.resizable(width=False, height=False)
-        self.config(bg='green')
+        #self.config(bg='green')
         self.title(program_name)
         self.iconbitmap(default='icon.ico')
 
@@ -22,10 +22,17 @@ class StartWindow(tk.Tk):
         self._buttons_height: int = 5
         self._buttons_font: int = 16
 
+        self._create_background()
         self._create_buttons()
 
     def run(self):
         self.mainloop()
+
+    def _create_background(self):
+        background_image = tk.PhotoImage(file='background.png')
+        background_label = tk.Label(self, image=background_image)
+        background_label.image = background_image
+        background_label.place(x=0, y=0, relheight=1, relwidth=1)
 
     def _create_buttons(self):
         self._create_add_doc_button()
